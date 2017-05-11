b59078b94e9311fea61c8a06e2016b3c2601e9fc
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-11 03:03:54 -0500

    Added title to indexes

--- source/index.ios.tsx
+++ source/index.ios.tsx
@@ -18,15 +18,11 @@ const styles: any = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
-  welcome: {
-    fontSize: 20,
+  title: {
+    fontSize: 64,
     textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+    margin: 10,
+    color: '#333'
   }
 });
 
@@ -34,15 +30,8 @@ export default class LiveWriter extends React.Component<Props, State> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native! (in TypeScript)
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text style={styles.title}>
+          LiveWriter
         </Text>
       </View>
     );
