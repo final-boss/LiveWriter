c59fdf0c73134a71636a7a25f3cda4fa3cfbe3b7
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-12 04:04:01 -0500

    Move index page to components/screens as landing screen

---
+++ source/components/screens/landing.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export interface Props { }
+export interface State { }
+
+const styles: any = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 64,
+    textAlign: 'center',
+    margin: 10,
+    color: '#333'
+  }
+});
+
+export default class LandingScreen extends React.Component<Props, State> {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          LiveWriter
+        </Text>
+      </View>
+    );
+  }
+}
