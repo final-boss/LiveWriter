15191f1ca0c9dd1b9d455a072f20541ec157e6a0
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-12 04:05:29 -0500

    Register container app in ios and android indexes

--- source/index.ios.tsx
+++ source/index.ios.tsx
@@ -1,41 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import { AppRegistry } from 'react-native';
+import { App }         from './app';
 
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-
-import React from 'react';
-
-export interface Props { }
-export interface State { }
-
-const styles: any = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  title: {
-    fontSize: 64,
-    textAlign: 'center',
-    margin: 10,
-    color: '#333'
-  }
-});
-
-export default class LiveWriter extends React.Component<Props, State> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          LiveWriter
-        </Text>
-      </View>
-    );
-  }
-}
-
-AppRegistry.registerComponent('LiveWriter', () => LiveWriter);
+AppRegistry.registerComponent('LiveWriter', () => App);
