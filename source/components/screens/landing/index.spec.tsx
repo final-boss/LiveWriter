f865ca899734d016b8787b7e7eb7a08ff6e54692
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-12 04:12:12 -0500

    Move landing screen to subfolder with index

--- source/components/screens/landing/index.spec.tsx
+++ source/components/screens/landing/index.spec.tsx
@@ -1,7 +1,7 @@
 import 'react-native';
 import 'jest';
 import React from 'react';
-import Landing from './landing';
+import Landing from './index';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
