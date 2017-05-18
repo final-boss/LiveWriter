ad2d16a3df8f3bf0a70b69b00c2252cf5f4f54a1
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-18 02:26:33 -0500

    Added canvas screen with spec

---
+++ source/components/screens/canvas/index.spec.tsx
@@ -0,0 +1,29 @@
+import 'react-native';
+import 'jest';
+import React        from 'react';
+import CanvasScreen from './index';
+import renderer     from 'react-test-renderer';
+
+describe(CanvasScreen.name, () => {
+
+  describe('render', () => {
+
+    const screen = renderer.create(
+      <CanvasScreen />
+    );
+
+    it('renders correctly', () => {
+      expect(screen).toBeDefined()
+    });
+  });
+
+  describe('.navigationOptions', () => {
+
+    const navigationOptions = CanvasScreen.navigationOptions;
+
+    it('does not have a header', () => {
+      expect(navigationOptions.header).toBeNull()
+    });
+  });
+
+});
