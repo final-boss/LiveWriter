b7afe156ef087cd310aa02f233f19873db6bbbc8
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-22 04:09:23 -0500

    Added paint view to canvas screen as canvas

--- source/components/screens/canvas/index.tsx
+++ source/components/screens/canvas/index.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Canvas               from '../../paint-view';
+import React                from 'react';
 
 export interface Props { }
 export interface State { }
@@ -7,15 +8,7 @@ export interface State { }
 const styles: any = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  title: {
-    fontSize: 64,
-    textAlign: 'center',
-    margin: 10,
-    color: '#333'
+    alignItems: 'stretch'
   }
 });
 
@@ -25,12 +18,15 @@ export default class CanvasScreen extends React.Component<Props, State> {
     header: null
   }
 
+  constructor(props, context) {
+    super(props, context);
+    this.state = { };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>
-          Canvas
-        </Text>
+        <Canvas />
       </View>
     );
   }
