b09cddfdcd468ee817ba12723719666242017b46
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-31 01:41:03 -0500

    Move app.tsx navigator to folder with index.ts

--- source/app/index.ts
+++ source/app/index.ts
@@ -1,7 +1,9 @@
 import { StackNavigator, NavigationContainer } from 'react-navigation';
-import LandingScreen   from './components/screens/landing';
-import NewCanvasScreen from './components/screens/new-canvas';
-import CanvasScreen    from './components/screens/canvas';
+import LandingScreen   from '../components/screens/landing';
+import NewCanvasScreen from '../components/screens/new-canvas';
+import CanvasScreen    from '../components/screens/canvas';
+
+console.log(StackNavigator);
 
 export const App : NavigationContainer = StackNavigator({
   Landing   : { screen : LandingScreen },
